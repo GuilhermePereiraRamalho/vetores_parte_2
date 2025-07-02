f19a31ed96532181b1c25b4f93a18ecc203f2d3e
Guilherme Ramalho <guilherme.p.ramalho@gmail.com>
2025-07-02 20:48:15 +0200

    Ajustando o program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import entities.Product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,16 +10,19 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        double[] vect = new double[n];
+        Product[] vect = new Product[n];
         for (int i=0; i<n; i++) {
-            vect[i] = sc.nextDouble();
+            sc.nextLine();
+            String name = sc.nextLine();
+            double price = sc.nextDouble();
+            vect[i] = new Product(name, price);
         }
         double sum = 0.0;
         for (int i=0; i<n; i++) {
-            sum += vect[i];
+            sum += vect[i].getPrice();
         }
         double avg = sum / n;
-        System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
+        System.out.printf("AVERAGE PRICE = %.2f%n", avg);
         sc.close();
     }
 }
